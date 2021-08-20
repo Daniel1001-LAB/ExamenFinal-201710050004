f0f5ae3d71b039fe2c576e7b0ea087d6ccd15c14
Daniuwu504 <74873220+Daniuwu504@users.noreply.github.com>
2021-08-20 12:56:36 -0600

    Add project files.

---
+++ ExamenTercerParcial/ExamenTercerParcial/Helpers/SampleColorType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamenTercerParcial.Helpers
+{
+    public enum SampleColorType
+    {
+        BackgroundGradientColor1,
+        BackgroundGradientColor2,
+        ShadowColor,
+        BorderColor
+    }
+}
---
+++ ExamenTercerParcial/ExamenTercerParcial/Helpers/SamplePointType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamenTercerParcial.Helpers
+{
+    public enum SamplePointType
+    {
+        BackgroundGradientStartPoint,
+        BackgroundGradientEndPoint,
+        BackgroundGradientStartPoint2,
+        BackgroundGradientEndPoint2,
+        BorderGradientStartPoint,
+        BorderGradientEndPoint
+    }
+}
